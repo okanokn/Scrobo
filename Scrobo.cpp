c8adddb0588e9e94b31bcaf4a726a05e135b687a
okanokn <17700248+okanokn@users.noreply.github.com>
2025-12-28 23:00:56 +0900

    Update to use TB6612FNG

--- Scrobo.cpp
+++ Scrobo.cpp
@@ -1,18 +1,26 @@
 /*
-  Scrobo.cpp - Library for Jikken 1, Theme 3.
-  Created by Kunihisa Okano, February 22, 2025.
-*/
-#include "Arduino.h"
+ * Scrobo.cpp - Library for Jikken 1, Theme 3.
+ * @author K. Okano
+ */
 #include "Scrobo.h"
 
-Scrobo::Scrobo() {
-}
+Scrobo::Scrobo() {}
 
-void Scrobo::begin(){
+void Scrobo::begin() {
   // Set pin modes
-  pinMode(M1DIR, OUTPUT);
-  pinMode(M2DIR, OUTPUT);
-  pinMode(M3DIR, OUTPUT);
+  pinMode(M1IN1, OUTPUT);
+  pinMode(M1IN2, OUTPUT);
+  pinMode(M1PWM, OUTPUT);
+  pinMode(M2IN1, OUTPUT);
+  pinMode(M2IN2, OUTPUT);
+  pinMode(M2PWM, OUTPUT);
+  pinMode(M3IN1, OUTPUT);
+  pinMode(M3IN2, OUTPUT);
+  pinMode(M2PWM, OUTPUT);
+  // Stop motors
+  analogWrite(M1PWM, 0);
+  analogWrite(M2PWM, 0);
+  analogWrite(M3PWM, 0);
 
   // pinMode(IRL, INPUT);
   // pinMode(IRC, INPUT);
@@ -22,45 +30,46 @@ void Scrobo::begin(){
   // pinMode(FLR, INPUT);
 }
 
-void Scrobo::motorSpeed(int m, int speed){
-    int pinDir;
-    int pinPWM;
-    switch (m){
-    case 1:
-        pinDir = M1DIR;
-        pinPWM = M1PWM;
-        break;
+void Scrobo::motorSpeed(int m, int speed) {
+  int pinDir1;
+  int pinDir2;
+  int pinPWM;
+  if (m == 1) {
+    pinDir1 = M1IN1;
+    pinDir2 = M1IN2;
+    pinPWM = M1PWM;
+  } else if (m == 2) {
+    pinDir1 = M2IN1;
+    pinDir2 = M2IN2;
+    pinPWM = M2PWM;
+  } else if (m == 3) {
+    pinDir1 = M3IN1;
+    pinDir2 = M3IN2;
+    pinPWM = M3PWM;
+  } else {
+    return;
+  }
 
-    case 2:
-        pinDir = M2DIR;
-        pinPWM = M2PWM;
-        break;
-
-    default:
-        pinDir = M3DIR;
-        pinPWM = M3PWM;
-        break;
-    }
-    if (speed < 0){
-        digitalWrite(pinDir, HIGH); // CCW
-        speed = (-1) * speed;
-    }else{
-        digitalWrite(pinDir, LOW); // CW
-    }
-    if (speed > MAXDUTY){
-        speed = MAXDUTY;
-    }
-    analogWrite(pinPWM, speed);
+  if (speed == 0) {
+    digitalWrite(pinDir1, HIGH);  // STOP
+    digitalWrite(pinDir2, HIGH);
+    analogWrite(pinPWM, 0);
+    return;
+  }
+  if (speed < 0) {
+    digitalWrite(pinDir1, LOW);   // CCW
+    digitalWrite(pinDir2, HIGH);  // CCW
+    speed = -speed;
+  } else {
+    digitalWrite(pinDir1, HIGH);  // CW
+    digitalWrite(pinDir2, LOW);   // CW
+  }
+  if (speed > MAXDUTY) {
+    speed = MAXDUTY;
+  }
+  analogWrite(pinPWM, speed);
 }
 
-void Scrobo::setSpeedM1(int speed){
-  motorSpeed(1, speed);
-}
-
-void Scrobo::setSpeedM2(int speed){
-  motorSpeed(2, speed);
-}
-
-void Scrobo::setSpeedM3(int speed){
-  motorSpeed(3, speed);
-}
+void Scrobo::setSpeedM1(int speed) { motorSpeed(1, speed); }
+void Scrobo::setSpeedM2(int speed) { motorSpeed(2, speed); }
+void Scrobo::setSpeedM3(int speed) { motorSpeed(3, speed); }
--- Scrobo.h
+++ Scrobo.h
@@ -2,12 +2,16 @@
 #define Scrobo_h
 #include "Arduino.h"
 
-#define M1PWM 5  //INA
-#define M1DIR 4  //INB
-#define M2PWM 6  //INA
-#define M2DIR 7  //INB
-#define M3PWM 9  //INA
-#define M3DIR 8  //INB
+#define M1PWM 5   // PWMA
+#define M1IN1 8   // AIN1
+#define M1IN2 7   // AIN2
+#define M2PWM 6   // PWMB
+#define M2IN1 11  // BIN1
+#define M2IN2 12  // BIN2
+#define M3PWM 9   // PWMA
+#define M3IN1 2   // AIN1
+#define M3IN2 4   // AIN2
+// STBY is set HIGH on the board
 
 // #define IRL A0
 // #define IRC A1
@@ -19,13 +23,13 @@
 #define MAXDUTY 255
 
 class Scrobo {
-  public:
-    Scrobo();
-    void begin();
-    void motorSpeed(int m, int speed);
-    void setSpeedM1(int speed);
-    void setSpeedM2(int speed);
-    void setSpeedM3(int speed);
+ public:
+  Scrobo();
+  void begin();
+  void motorSpeed(int m, int speed);
+  void setSpeedM1(int speed);
+  void setSpeedM2(int speed);
+  void setSpeedM3(int speed);
 };
 
 #endif
