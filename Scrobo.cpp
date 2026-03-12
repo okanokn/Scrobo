1e21880bd1b65113646008d32fe8896c3ad99ff8
okanokn <17700248+okanokn@users.noreply.github.com>
2026-03-13 04:14:54 +0900

    Update to use DFRobot DRI0044

--- Scrobo.cpp
+++ Scrobo.cpp
@@ -8,19 +8,19 @@ Scrobo::Scrobo() {}
 
 void Scrobo::begin() {
   // Set pin modes
-  pinMode(M1IN1, OUTPUT);
-  pinMode(M1IN2, OUTPUT);
+  pinMode(M1DIR, OUTPUT);
   pinMode(M1PWM, OUTPUT);
-  pinMode(M2IN1, OUTPUT);
-  pinMode(M2IN2, OUTPUT);
+  pinMode(M2DIR, OUTPUT);
   pinMode(M2PWM, OUTPUT);
-  pinMode(M3IN1, OUTPUT);
-  pinMode(M3IN2, OUTPUT);
+  pinMode(M3DIR, OUTPUT);
   pinMode(M2PWM, OUTPUT);
+  pinMode(M4DIR, OUTPUT);
+  pinMode(M4PWM, OUTPUT);
   // Stop motors
   analogWrite(M1PWM, 0);
   analogWrite(M2PWM, 0);
   analogWrite(M3PWM, 0);
+  analogWrite(M4PWM, 0);
 
   // pinMode(IRL, INPUT);
   // pinMode(IRC, INPUT);
@@ -31,38 +31,34 @@ void Scrobo::begin() {
 }
 
 void Scrobo::motorSpeed(int m, int speed) {
-  int pinDir1;
-  int pinDir2;
+  int pinDir;
   int pinPWM;
   if (m == 1) {
-    pinDir1 = M1IN1;
-    pinDir2 = M1IN2;
+    pinDir = M1DIR;
     pinPWM = M1PWM;
   } else if (m == 2) {
-    pinDir1 = M2IN1;
-    pinDir2 = M2IN2;
+    pinDir = M2DIR;
     pinPWM = M2PWM;
   } else if (m == 3) {
-    pinDir1 = M3IN1;
-    pinDir2 = M3IN2;
+    pinDir = M3DIR;
     pinPWM = M3PWM;
+  } else if (m == 4) {
+    pinDir = M4DIR;
+    pinPWM = M4PWM;
   } else {
     return;
   }
 
-  if (speed == 0) {
-    digitalWrite(pinDir1, HIGH);  // STOP
-    digitalWrite(pinDir2, HIGH);
-    analogWrite(pinPWM, 0);
-    return;
-  }
+  // if (speed == 0) {
+  //   digitalWrite(pinDir, HIGH);  // STOP
+  //   analogWrite(pinPWM, 0);
+  //   return;
+  // }
   if (speed < 0) {
-    digitalWrite(pinDir1, LOW);   // CCW
-    digitalWrite(pinDir2, HIGH);  // CCW
+    digitalWrite(pinDir, HIGH);  // CCW
     speed = -speed;
   } else {
-    digitalWrite(pinDir1, HIGH);  // CW
-    digitalWrite(pinDir2, LOW);   // CW
+    digitalWrite(pinDir, LOW);  // CW
   }
   if (speed > MAXDUTY) {
     speed = MAXDUTY;
@@ -73,3 +69,4 @@ void Scrobo::motorSpeed(int m, int speed) {
 void Scrobo::setSpeedM1(int speed) { motorSpeed(1, speed); }
 void Scrobo::setSpeedM2(int speed) { motorSpeed(2, speed); }
 void Scrobo::setSpeedM3(int speed) { motorSpeed(3, speed); }
+void Scrobo::setSpeedM4(int speed) { motorSpeed(4, speed); }
--- Scrobo.h
+++ Scrobo.h
@@ -2,15 +2,14 @@
 #define Scrobo_h
 #include "Arduino.h"
 
-#define M1PWM 5   // PWMA
-#define M1IN1 8   // AIN1
-#define M1IN2 7   // AIN2
-#define M2PWM 6   // PWMB
-#define M2IN1 11  // BIN1
-#define M2IN2 12  // BIN2
-#define M3PWM 9   // PWMA
-#define M3IN1 2   // AIN1
-#define M3IN2 4   // AIN2
+#define M1PWM 6
+#define M1DIR 7
+#define M2PWM 5
+#define M2DIR 4
+#define M3PWM 9
+#define M3DIR 8
+#define M4PWM 10
+#define M4DIR 11
 // STBY is set HIGH on the board
 
 // #define IRL A0
@@ -30,6 +29,7 @@ class Scrobo {
   void setSpeedM1(int speed);
   void setSpeedM2(int speed);
   void setSpeedM3(int speed);
+  void setSpeedM4(int speed);
 };
 
 #endif
